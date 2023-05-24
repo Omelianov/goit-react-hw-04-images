16df27a04244a20d0ae4b0dde5e00448055b24bc
Omelianov Nicolai <omelianov.n@gmail.com>
2023-05-24 11:13:03 +0300

    foxed modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,37 +2,36 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+export const Modal = ({ data, onToggleModal }) => {
+  const { source, alt } = data;
 
-export const Modal = ({data, onToggleModal}) =>  {
-    const [source, alt] = data;
+  useEffect(() => {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      onToggleModal();
+    }
+  };
 
+  window.addEventListener('keydown', handleKeyDown);
 
-    useEffect(() => {
-      const handleKeyDown = (event) => {
-        if (event.key === 'Escape') {
-            onToggleModal();
-        }
-      };
-      window.addEventListener("keydown", handleKeyDown);
-    
-      return () => {
-        window.removeEventListener("keydown", handleKeyDown);
-      };
-    }, [onToggleModal]);
+  return () => {
+    window.removeEventListener('keydown', handleKeyDown);
+  };
+}, [onToggleModal]);
 
-        return (
-            <div className="Overlay" onClick={onToggleModal}>
-                <div className="Modal">
-                    <img src={source} alt={alt} />
-                </div>
-            </div>
-        );
-    };
+  return (
+    <div className="Overlay" onClick={onToggleModal} tabIndex={0}>
+      <div className="Modal">
+        <img src={source} alt={alt} />
+      </div>
+    </div>
+  );
+};
 
-    Modal.propTypes = {
-        data: PropTypes.shape({
-            source: PropTypes.string.isRequired,
-            alt: PropTypes.string.isRequired,
-        }).isRequired,
-        onToggleModal: PropTypes.func.isRequired,
-    };
+Modal.propTypes = {
+  data: PropTypes.shape({
+    source: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }).isRequired,
+  onToggleModal: PropTypes.func.isRequired,
+};
